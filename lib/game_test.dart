1ced2ff393542451d0219a1da0b946bad49de318
lefarmer01600 <t.clemenceau@outlook.fr>
2024-12-09 10:25:52 +0100

    Update game assets and enhance grid layout functionality

--- lib/game_test.dart
+++ lib/game_test.dart
@@ -3,54 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:async'; //Pour le timer
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const GameTest(gameNumber: 0,));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GameTest extends StatelessWidget {
+  const GameTest({super.key, required this.gameNumber});
+
+  final int gameNumber;
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'Ou est Charlie';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              const Text(appTitle),
-              const SizedBox(width: 5),
-              Image.asset(
-                'images/waldo.jpg',
-                width: 30,
-                height: 30,
-              ),
-              const SizedBox(width: 0),
-              Image.asset(
-                'images/odlaw.jpg',
-                width: 30,
-                height: 30,
-              ),
-              const SizedBox(width: 0),
-              Image.asset(
-                'images/wizard.jpg',
-                width: 30,
-                height: 30,
-              ),
-            ],
-          ),
-        ),
-        body: const Column(
-          children: [
-            ImageSection()
-          ],
-        ),
-      ),
-    );
+    return ImageSection(gameNumber: gameNumber);
   }
 }
 
 class ImageSection extends StatefulWidget {
-  const ImageSection({super.key});
+  const ImageSection({super.key, required this.gameNumber});
+
+  final int gameNumber;
 
   @override
   State<ImageSection> createState() => _ImageSectionState();
@@ -59,31 +28,40 @@ class ImageSection extends StatefulWidget {
 class _ImageSectionState extends State<ImageSection> {
   List jsonList = [];
   int _score = 0;
-  int gameNumber = 0;
+  // int gameNumber = 0;
+  late int gameNumber;
   late Timer _timer;
   int _elapsedSeconds = 0;
 
   @override
   void initState() {
     super.initState();
+    gameNumber = widget.gameNumber;
     _loadJsonData();
     _startTimer();
   }
 
   Future<void> _loadJsonData() async {
-      String jsonString = await rootBundle.loadString('assets/games.json');
-      final jsonData = json.decode(jsonString);
+    String jsonString = await rootBundle.loadString('assets/games.json');
+    final jsonData = json.decode(jsonString);
 
-      setState(() {
-        jsonList = jsonData;
-      });
-
+    setState(() {
+      jsonList = jsonData;
+    });
   }
-    void _startTimer() {
+
+  void _startTimer() {
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         _elapsedSeconds++;
       });
+    });
+  }
+
+  void _resetScore() {
+    setState(() {
+      _score = 0;
+      _elapsedSeconds = 0;
     });
   }
 
@@ -95,8 +73,8 @@ class _ImageSectionState extends State<ImageSection> {
     _showMessage(context, '$characterName trouvé !');
 
     // Tous trouvé
-    if (_score == 3) {
-      _timer.cancel(); 
+    if (_score == jsonList[gameNumber]["perso"].length) {
+      _timer.cancel();
       final minutes = (_elapsedSeconds ~/ 60).toString().padLeft(2, '0');
       final seconds = (_elapsedSeconds % 60).toString().padLeft(2, '0');
       String time = '$minutes:$seconds';
@@ -106,25 +84,45 @@ class _ImageSectionState extends State<ImageSection> {
         builder: (context) {
           int adjustedScore;
           if (_elapsedSeconds <= 30) {
-            adjustedScore = 3;
+            adjustedScore = 3000;
           } else if (_elapsedSeconds <= 90) {
-            adjustedScore = 2;
+            adjustedScore = 2000;
           } else {
-            adjustedScore = 1;
+            adjustedScore = 1000;
           }
           return AlertDialog(
             title: const Text('Bravo ! 🎉'),
-            content: Text('Vous avez trouvé tous les personnages en $time ! \n' 'Votre score final est : $adjustedScore/3'),
+            content: Text('Vous avez trouvé tous les personnages en $time ! \n'
+                'Votre score final est : $adjustedScore pts'),
             actions: [
               TextButton(
                 onPressed: () {
                   Navigator.of(context).pop();
                   Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => const MyApp()),
-                  ); // Redémarrer le jeu
+                    MaterialPageRoute(
+                        builder: (context) => GameTest(gameNumber: gameNumber)),
+                  );
+                  _resetScore();
+                  _loadJsonData();
                 },
                 child: const Text('Recommencer'),
-                // child: const Text('Prochain niveau') 
+              ),
+              TextButton(
+                onPressed: () {
+                  if (gameNumber + 1 < jsonList.length) {
+                    // Ensure we don't exceed available levels
+                    setState(() {
+                      gameNumber++; // Move to the next level
+                      _score = 0; // Reset score for the new level
+                      _elapsedSeconds = 0; // Reset timer for the new level
+                    });
+                  } else {
+                    // Handle if no more levels are available (optional)
+                    _showMessage(context, 'Plus de niveaux disponibles !');
+                  }
+                  Navigator.of(context).pop(); // Close the dialog
+                },
+                child: const Text('Prochain niveau'),
               ),
             ],
           );
@@ -135,36 +133,87 @@ class _ImageSectionState extends State<ImageSection> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: InteractiveViewer(
-        scaleEnabled: true,
-        panEnabled: true,
-        minScale: 0.5,
-        maxScale: 3,
-        constrained: false, // Allow unrestricted child size
-        boundaryMargin: EdgeInsets.zero,
-        child: GestureDetector(
-          onTapDown: (TapDownDetails details) {
+    const String appTitle = 'Ou est Charlie';
+    return MaterialApp(
+        title: appTitle,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Row(
+                children: [
+                  const Text(appTitle),
+                  const SizedBox(width: 5),
+                
+                  ...(jsonList[gameNumber]["perso"] as List)
+                      .map<Widget>((element) {
+                        print(element);
+                    if (!element["found"]) {
+                      return Image.asset(
+                        'images/${element["image"]}',
+                        width: 30,
+                        height: 30,
+                      );
+                    } else {
+                      return Image.asset(
+                        'images/${element["image"]}',
+                        width: 30,
+                        height: 30,
+                        color: Colors.grey,
+                      );
+                    }
+                  }),
 
-            final offset = details.localPosition;
-            
-            jsonList[gameNumber]["perso"].forEach((element) {
-              if (_checkIfFound(element["coords"], offset)) {
-                _incrementScore(context, element["name"]);
-              }
-            });
+                  // Image.asset(
+                  //   'images/waldo.jpg',
+                  //   width: 30,
+                  //   height: 30,
+                  // ),
+                  // const SizedBox(width: 0),
+                  // Image.asset(
+                  //   'images/odlaw.jpg',
+                  //   width: 30,
+                  //   height: 30,
+                  // ),
+                  // const SizedBox(width: 0),
+                  // Image.asset(
+                  //   'images/wizard.jpg',
+                  //   width: 30,
+                  //   height: 30,
+                  // ),
+                ],
+              ),
+            ),
+            body: Expanded(
+              child: InteractiveViewer(
+                scaleEnabled: true,
+                panEnabled: true,
+                minScale: 0.5,
+                maxScale: 3,
+                constrained: false,
+                boundaryMargin: EdgeInsets.zero,
+                child: GestureDetector(
+                  onTapDown: (TapDownDetails details) {
+                    final offset = details.localPosition;
 
-            print("Clic détecté aux coordonnées : ${offset.dx}, ${offset.dy}");
-          },
-          child: Image.asset(
-            'images/${jsonList[gameNumber]["image"]}',
-            width: 1000,
-            height: 1000,
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
-    );
+                    jsonList[gameNumber]["perso"].forEach((element) {
+                      if (_checkIfFound(element["coords"], offset) &&
+                          !element["found"]) {
+                        _incrementScore(context, element["name"]);
+                        element["found"] = true;
+                      }
+                    });
+
+                    print(
+                        "Clic détecté aux coordonnées : ${offset.dx}, ${offset.dy}");
+                  },
+                  child: Image.asset(
+                    'images/${jsonList[gameNumber]["image"]}',
+                    // width: 1000,
+                    // height: 1000,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            )));
   }
 
   void _showMessage(BuildContext context, String message) {
@@ -172,9 +221,12 @@ class _ImageSectionState extends State<ImageSection> {
       SnackBar(content: Text(message)),
     );
   }
-  
+
   bool _checkIfFound(final waldoCoords, final offset) {
-    return ((waldoCoords["x"] + 20) > offset.dx && offset.dx > (waldoCoords["x"] - 30)) && ((waldoCoords["y"] + 20) > offset.dy && offset.dy > (waldoCoords["y"] - 30));
+    return ((waldoCoords["x"] + 20) > offset.dx &&
+            offset.dx > (waldoCoords["x"] - 30)) &&
+        ((waldoCoords["y"] + 20) > offset.dy &&
+            offset.dy > (waldoCoords["y"] - 30));
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:convert'; // Pour décoder le JSON
 import 'package:flutter/material.dart';
-// import 'gameex.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+import 'game_test.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -20,36 +24,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CustomGrid extends StatelessWidget {
-
+class CustomGrid extends StatefulWidget {
   const CustomGrid({super.key});
+
+  @override
+  State<CustomGrid> createState() => _CustomGridState();
+}
+
+class _CustomGridState extends State<CustomGrid> {
+  List jsonList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadJsonData();
+  }
+
+  Future<void> _loadJsonData() async {
+    String jsonString = await rootBundle.loadString('assets/games.json');
+    final jsonData = json.decode(jsonString);
+
+    setState(() {
+      jsonList = jsonData;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // First row with one item spanning two columns
-        // Container(
-        //   color: Colors.blue,
-        //   height: 100,
-        //   width: double.infinity,
-        //   alignment: Alignment.center,
-        //   child: const Text(
-        //     'Item 1',
-        //     style: TextStyle(color: Colors.white, fontSize: 20),
-        //   ),
-        // ),
-        // Grid with two columns
         Expanded(
           child: GridView.count(
             crossAxisCount: 2,
-            children:List.generate(10, (index) {
-            return Center(
-              child: ItemWidget(
-                text : 'Item $index'
-              ),
-            );
-          }),
+            children: List.generate(jsonList.length, (index) {
+              return Center(
+                child: ItemWidget(
+                  text: jsonList[index]['name'],
+                  image: jsonList[index]['image'],
+                  index: index,
+                ),
+              );
+            }),
           ),
         ),
       ],
@@ -61,24 +76,52 @@ class ItemWidget extends StatelessWidget {
   const ItemWidget({
     super.key,
     required this.text,
+    required this.image,
+    required this.index,
   });
 
   final String text;
+  final String image;
+  final int index;
 
   @override
   Widget build(BuildContext context) {
-    return 
-    GestureDetector(
+    return GestureDetector(
       onTap: () {
         print('Item $text clicked');
-        // Navigator.push(
-        //       context,
-        //       MaterialPageRoute(builder: (context) => const Game1()),
-        //     );
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => GameTest(gameNumber: index,)),
+        );
       },
       child: Card(
         child: SizedBox(
-          child: Center(child: Text(text)),
+          child: Stack(
+            children: [
+              ColorFiltered(
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.5),
+                    BlendMode.darken,
+                  ),
+                  child: Image.asset(
+                    'images/$image',
+                    width: double.infinity,
+                    height: double.infinity,
+                    fit: BoxFit.cover,
+                  )),
+              Center(
+                child: Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
